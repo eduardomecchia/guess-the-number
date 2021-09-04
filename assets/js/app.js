578ffc82cc3442fced6eed52c05d79f1c3b75c8e
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-09-04 18:08:10 +0200

    Change feedback field based on what the user clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,24 @@ const buttonsEl = document.querySelector('.buttons');
 for (let i = 0; i < 10; i++) {
     buttonsEl.innerHTML += `<button class="user-button">${i}</button>`;
 }
+
+// Add functionality to buttons on click
+function chooseNumber(element) {
+    return element.innerHTML;
+}
+
+const allButtons = document.querySelectorAll('.user-button');
+
+// Change feedback field based on what the user clicked
+allButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        if (chooseNumber(button) == randomNumber) {
+            feedbackEl.innerHTML = 'Esatto!';
+        } else if (chooseNumber(button) < randomNumber) {
+            feedbackEl.innerHTML = 'Più alto!';
+        } else if (chooseNumber(button) > randomNumber) {
+            feedbackEl.innerHTML = 'Più basso!';
+        }
+    });
+});
+
