515670d09455133b8a702ae936187a880482d30d
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-09-04 21:01:07 +0200

    Hide system topbar in main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
     const window = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      frame: false
     });
 
     // Load index.html
