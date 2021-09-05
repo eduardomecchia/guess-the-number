4de4a4198009b3344b9943016e04de1aff65b31b
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-09-05 23:14:12 +0200

    Implement almost all the logic behind reversed mode

--- assets/js/reversed.js
+++ assets/js/reversed.js
@@ -4,6 +4,8 @@ const lowerButton = document.querySelector('#lower');
 const correctButton = document.querySelector('#correct');
 const computerNumberEl = document.querySelector('.computer-number');
 
+let activeGame = false;
+
 function computerGuess() {
     return Math.floor(Math.random() * 10);
 }
@@ -16,6 +18,48 @@ const updateComputerField = () => {
 const beginButton = document.querySelector('#begin');
 
 beginButton.addEventListener('click', function () {
-    updateComputerField();
+    if (!activeGame) {
+        activeGame = true;
+        updateComputerField();
+    }
 });
 
+// When you press 'Higher', the computer guesses a higher number
+higherButton.addEventListener('click', function () {
+    if (activeGame) {
+        let oldComputerNumber = computerNumberEl.innerHTML.slice(-1);
+        let newComputerNumber = 0;
+        newComputerNumber = computerGuess();
+        
+        while (newComputerNumber <= oldComputerNumber) {
+            if (newComputerNumber == 9) return;
+            newComputerNumber = computerGuess();
+        }
+    
+        computerNumberEl.innerHTML = 'Il computer ha scelto il numero: ' + newComputerNumber;
+    }
+});
+
+// When you press 'Lower', the computer guesses a lower number
+lowerButton.addEventListener('click', function () {
+    if (activeGame) {
+        let oldComputerNumber = computerNumberEl.innerHTML.slice(-1);
+        let newComputerNumber = 0;
+        newComputerNumber = computerGuess();
+    
+        while (newComputerNumber >= oldComputerNumber) {
+            if (newComputerNumber == 0) return;
+            newComputerNumber = computerGuess();
+        }
+    
+        computerNumberEl.innerHTML = 'Il computer ha scelto il numero: ' + newComputerNumber;
+    }
+});
+
+// When you press 'Correct', the computer wins
+correctButton.addEventListener('click', function () {
+    if (activeGame) {
+        computerNumberEl.innerHTML = 'Ci ho azzeccato!';
+        activeGame = false;
+    }
+});
